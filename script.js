0b1543e49d87d4a5ce9ceb3c8b192b650ea17248
wojeee <rosameera@borneodigital.co>
2023-09-14 11:24:18 +0800

    try add on clicked bttn

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
 
-btn.onclick = function () {
+/*btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
 	number += Math.ceil(Math.random() * 1000);
+}*/
+btn.onclick = function () {
+    console.log("Button clicked!");  // This should print in the console when the button is clicked
+    container.style.transform = "rotate(" + number + "deg)";
+    number += Math.ceil(Math.random() * 1000);
 }
